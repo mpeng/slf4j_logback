b3d452ced0151ff6c9c2583321a32b9e7eb3c411
wensheng.peng <wensheng.peng@cotiviti.com>
2024-04-12 20:49:37 -0400

    Original commit

--- slf4j-logback/src/main/java/com/mkyong/HelloWorld.java
+++ slf4j-logback/src/main/java/com/mkyong/HelloWorld.java
@@ -5,14 +5,29 @@ import org.slf4j.LoggerFactory;
 
 public class HelloWorld {
 
-    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+    private final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 
     public static void main(String[] args) {
 
-        logger.debug("Hello from Logback");
+        System.setProperty("log.name",
+          "C:\\Work\\PCAICMSBATCH\\ICMS_Batch\\business_processes\\qa2\\temp\\himrk_daily_incoming_15p20230923.09-23-2023_0503.03-27-2024_1004_qa2_java_log.1");
+
+        new HelloWorld().logger.debug("Hello from Logback");
 
         //while (true)//test rotate file
-        logger.debug("getNumber() : {}", getNumber());
+        new HelloWorld().logger.debug("getNumber() : {}", getNumber());
+
+        /*
+        new HelloWorld().logger.trace("This is a TRACE message.");
+        new HelloWorld().logger.debug("This is a DEBUG message.");
+        new HelloWorld().logger.info("This is an INFO message.");
+        new HelloWorld().logger.warn("This is a WARN message.");
+        new HelloWorld().logger.error("This is an ERROR message.", new RuntimeException("Sample Exception"));
+        */
+        // Using parameterized logging
+        String name = "John";
+        int age = 30;
+        new HelloWorld().logger.info("User {} is {} years old.", name, age);
 
     }
 
