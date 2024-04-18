82cf800991028e760ff52693edd5c6c789384fda
wensheng.peng <wensheng.peng@cotiviti.com>
2024-04-17 20:42:14 -0400

    Write to a file dynamically but with a different logger

--- slf4j-logback/src/main/java/com/mkyong/HelloWorld.java
+++ slf4j-logback/src/main/java/com/mkyong/HelloWorld.java
@@ -6,24 +6,43 @@ import org.slf4j.LoggerFactory;
 public class HelloWorld {
 
     private final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+    private static Logger myLogger = LoggerFactory.getLogger(LogbackFileUtils.MY_LOGGER);
+    private static Logger icmsLogger = LoggerFactory.getLogger(LogbackFileUtils.ICMS);
 
     public static void main(String[] args) {
 
+
         System.setProperty("log.name",
           "C:\\Work\\PCAICMSBATCH\\ICMS_Batch\\business_processes\\qa2\\temp\\himrk_daily_incoming_15p20230923.09-23-2023_0503.03-27-2024_1004_qa2_java_log.2");
+
+        icmsLogger.info( "Welcome" );
+
+        LogbackFileUtils.start("first.log");
+        myLogger.info("1st file - This is an info message");
+        myLogger.debug("1st file - This is a debug message");
+        myLogger.error("1st file - This is an error message");
+        LogbackFileUtils.stop();
+
+        LogbackFileUtils.start("second.log");
+        myLogger.info("2nd file - This is an info message");
+        myLogger.debug("2nd file - This is a debug message");
+        myLogger.error("2nd file - This is an error message");
+        LogbackFileUtils.stop();
+
+
 
         new HelloWorld().logger.debug("Hello from Logback");
 
         //while (true)//test rotate file
         new HelloWorld().logger.debug("getNumber() : {}", getNumber());
 
-        /*
+
         new HelloWorld().logger.trace("This is a TRACE message.");
         new HelloWorld().logger.debug("This is a DEBUG message.");
         new HelloWorld().logger.info("This is an INFO message.");
         new HelloWorld().logger.warn("This is a WARN message.");
         new HelloWorld().logger.error("This is an ERROR message.", new RuntimeException("Sample Exception"));
-        */
+
         // Using parameterized logging
         String name = "John";
         int age = 30;
---
+++ slf4j-logback/src/main/java/com/mkyong/LogbackFileUtils.java
@@ -0,0 +1,56 @@
+package com.mkyong;
+
+import ch.qos.logback.classic.Level;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.FileAppender;
+
+class LogbackFileUtils {
+
+  public static final String MY_LOGGER = "MY_LOGGER";
+  public static final String ICMS = "com.mkyong";
+  private static FileAppender<ILoggingEvent> fileAppender;
+  private static boolean initialized = false;
+
+  public static void init() {
+    if (!initialized) {
+      LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+      Logger myLogger = loggerContext.getLogger(MY_LOGGER);
+      Logger icmsLogger = loggerContext.getLogger(ICMS);
+      System.out.println( "========================myLogger======" + myLogger );
+      System.out.println( "========================icmsLogger======" + icmsLogger );
+
+      PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+      encoder.setContext(loggerContext);
+      encoder.setPattern("%d{HH:mm:ss.SSS} [%-5level] %msg %n");
+      encoder.start();
+
+      fileAppender = new FileAppender<ILoggingEvent>();
+      fileAppender.setContext(loggerContext);
+      fileAppender.setName("MY_FILE_LOGGER");
+      fileAppender.setAppend(true);
+      fileAppender.setEncoder(encoder);
+      myLogger.addAppender(fileAppender);
+      myLogger.setLevel(Level.ALL);
+
+      initialized = true;
+    }
+  }
+
+  public static void start(String filePath) {
+    init();
+    stop();
+    fileAppender.setFile(filePath);
+    fileAppender.start();
+  }
+
+  public static void stop() {
+    if (fileAppender.isStarted()) {
+      fileAppender.stop();
+    }
+  }
+}
